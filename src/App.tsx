7c5a7162f136e7f0d5cd4e062bbd1254a978e33e
Riccardo Consolandi <rc@capsula.app>
2023-02-05 13:46:12 +0100

    refactor: app; type, refactoring of styling add imgs
    	- add imgs for horses
    
    	- refactoring of style WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,75 @@
-import React from "react";
 import style from "./app.module.css";
+import horse from "./api/horseApi.json";
+import { horseType } from "./customType/type";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const images: any = {
+    appaloosa: require("./img/appaloosa.png"),
+  };
+  const [expanded, setExpanded] = useState(false);
+
+  const horseBreed: any = horse.horse_breed;
+
   return (
     <div className="App">
-      <h1 className={style.title}>Hi there</h1>
+      {horseBreed.map((items: horseType) => {
+        if (items.name) {
+          return (
+            <div key={items.id} className={style.flex}>
+              <div>
+                <img
+                  src={images[items.name.toLowerCase()]}
+                  alt={items.name}
+                  className={style.img}
+                />
+              </div>
+              <div className={style.info}>
+                <p className={style.detail}>
+                  <span className={style.title}>Name: </span>
+                  <span className={style.rightText}>{items.name}</span>
+                </p>
+                <p className={style.detail}>
+                  <span className={style.title}>Country of Origin: </span>
+                  <span className={style.rightText}>{items.country}</span>
+                </p>
+                <p className={style.detail}>
+                  <span className={style.title}>Weight: </span>
+                  <span className={style.rightText}>{items.weight}</span>
+                </p>
+                <p className={style.detail}>
+                  <span className={style.title}>Body Type: </span>
+                  <span className={style.rightText}>{items.body_type}</span>
+                </p>
+                <p className={style.detail}>
+                  <span className={style.title}>Life Expectancy: </span>
+                  <span className={style.rightText}>
+                    {items.life_expectancy}
+                  </span>
+                </p>
+                <div
+                  className={`${style.detail}`}
+                  onClick={() => setExpanded(!expanded)}
+                >
+                  <span className={style.title}>Personality: </span>
+                  <span className={style.arrow}>&#x25B6;</span>
+                  <div
+                    className={`${style.info} ${
+                      expanded ? style.expanded : style.collapsed
+                    }`}
+                  >
+                    {items.personality}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/customType/type.ts
@@ -0,0 +1,38 @@
+export interface horseType {
+  id?: number;
+  name?: string;
+  img?: string;
+  country?: string;
+  weight?: string;
+  body_type?: string;
+  life_expectancy?: number;
+  colour?: {
+    Blanket: string[];
+    Leopard: string[];
+    Snowflake: string[];
+    "Marble/Varnish": string[];
+  };
+  personality?: string;
+}
+
+export interface horseT {
+  horse_breed: HorseBreed[];
+}
+
+export interface HorseBreed {
+  id: number;
+  name: string;
+  img: string;
+  country: string;
+  weight: string;
+  body_type: string;
+  life_expectancy: number;
+  colour: Colour;
+}
+
+export interface Colour {
+  Blanket: string[];
+  Leopard: string[];
+  Snowflake: string[];
+  "Marble/Varnish": string[];
+}
